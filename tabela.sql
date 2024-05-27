8b9f61b8a9cbe3ab49d4a56e95d73fd8a200d26c
DiegoAlves1984 <diegoauves@hotmail.com>
2024-05-27 19:56:17 -0300

    adicionar

---
+++ tabela.sql
@@ -0,0 +1,13 @@
+CREATE TABLE Loyalty 
+(
+    ID_cliente NOT NULL INT PRIMARY KEY,
+    Sobrenome_cliente VARCHAR(50),
+    Nome_cliente VARCHAR(50),
+    Numero_fidelidade INTEGER NOT NULL VARCHAR(20),
+    Numero_contato VARCHAR(20),
+    Email VARCHAR(100),
+    Saldo_pontos INTEGER
+);
+
+INSERT INTO Loyalty
+(1, "Alves", "Diego", "00001", "83999277456", "diegoalves@hotmail.com", "10000")
